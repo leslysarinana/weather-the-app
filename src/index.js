4f035be1fea66d69f9c0953c3cab3c39f38b7e68
leslysarinana <leslysarinana2@gmail.com>
2024-02-28 22:54:19 -0600

    Injected the forecast HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ function refreshWeather(response) {
   let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
   
-  
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
@@ -53,7 +52,38 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast(){
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day){
+   forecastHtml =
+   forecastHtml + 
+   `
+     <div class="weather-forecast-day">
+      <div class="weather-forecast-date">${day}</div>
+      <div class="weather-forecast-icon">🌥️</div>
+      <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">
+        <strong>15°</strong>
+      </div>
+      <div class="weather-forecast-temperature">9°</div>
+      </div>
+   </div>
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+
+ forecastElement.innerHTML = forecastHtml;
+}
+
+  forecastElement = document.querySelector("#forecast");
+
+ 
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Dallas");
+displayForecast();
